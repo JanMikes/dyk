1a097ab1cf898511c11303804513c649dcd1b681
Jan Mikeš <j.mikes@me.com>
2019-09-23 13:18:22 +0200

    Router is no longer specific for domain

--- app/Router/RouterFactory.php
+++ app/Router/RouterFactory.php
@@ -12,19 +12,17 @@ final class RouterFactory
 {
     use Nette\StaticClass;
 
-    private const BASE_URL = '//www.ph30.eu/';
-
     public static function createRouter(): RouteList
     {
         $router = new RouteList;
 
-        $router->addRoute(self::BASE_URL . 'ADMIN/<presenter>/<action>[/<id>]', [
+        $router->addRoute('ADMIN/<presenter>/<action>[/<id>]', [
             'module' => 'Admin',
             'presenter' => 'Homepage',
             'action'    => 'default'
         ]);
 
-        $router->addRoute(self::BASE_URL . '<presenter>/<action>[/<id>]', [
+        $router->addRoute('<presenter>/<action>[/<id>]', [
             'presenter' => 'Homepage',
             'action'    => 'default'
         ]);
